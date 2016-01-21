2e513addaf4e783be0a08aa3b20a678a21cc7503
yoshiomi999 <yoshiomi999@gmail.com>
2016-01-21 13:14:18 +0000

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.create!(name: "Book",
+             basic_tax: false,
+             duty: false,
+             price: 12.49)
+             
+Item.create!(name: "Music CD",
+             basic_tax: true,
+             duty: false,
+             price: 14.99)
+             
+Item.create!(name: "Chocolate bar",
+             basic_tax: false,
+             duty: false,
+             price: 0.85)
+
+Item.create!(name: "Imported box of chocolates",
+             basic_tax: false,
+             duty: true,
+             price: 10)
+
+Item.create!(name: "Imported bottle of perfume",
+             basic_tax: true,
+             duty: true,
+             price: 47.5)
+
+Item.create!(name: "Imported bottle of perfume",
+             basic_tax: true,
+             duty: true,
+             price: 27.99)
+
+Item.create!(name: "Bottle of perfume",
+             basic_tax: true,
+             duty: false,
+             price: 18.99)
+
+Item.create!(name: "Packet of headache pills",
+             basic_tax: false,
+             duty: false,
+             price: 9.75)
+
+Item.create!(name: "Box of imported chocolates",
+             basic_tax: false,
+             duty: true,
+             price: 11.25)
